cd609ea74d95d8e998a6a662f0dd44a1861f98f6
shariyerShazan <shariyershazan1@gmail.com>
2025-08-10 23:02:13 +0600

    custom color and navbar making

--- src/componenets/Navbar.jsx
+++ src/componenets/Navbar.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import {links} from "../data"
+import { NavLink } from 'react-router'
 
 function Navbar() {
   return (
     <div>
-      nav
+        <nav>
+            <div className='fixed bg-container w-full px-14 py-8'>
+                <ul>
+                    {
+                        links.map((data , index)=>{
+                             return <li key={index}>
+                                <NavLink to={data.path} className={({isActive})=> `flex items-center text-lg gap-2 ${isActive? "" : ""}`} to={data?.path}>
+                                     {data?.icon}
+                                     {data?.name}
+                                </NavLink>
+                             </li>
+                        })
+                    }
+                </ul>
+            </div>
+        </nav>
+      
     </div>
   )
 }
