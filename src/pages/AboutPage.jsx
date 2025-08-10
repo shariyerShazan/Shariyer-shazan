65a1c9e63327a20d82480158ddad1a8266b7c2ca
shariyerShazan <shariyershazan1@gmail.com>
2025-08-11 02:39:07 +0600

    about pages

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,9 +1,137 @@
 import React from 'react'
+import { FaArrowRight } from 'react-icons/fa'
+import { FaFacebookF, FaGithub, FaLinkedinIn } from 'react-icons/fa6'
+import { HiOutlineMail } from 'react-icons/hi'
 
 function AboutPage() {
   return (
-    <div>
-      about
+    <div className="max-w-7xl mx-auto px-6 py-12">
+    
+      <h2 className="text-2xl md:text-5xl font-bold text-center py-8 border-b-2 border-gray-400">
+        About <span className="text-primary">Shariyer Shazan</span>
+      </h2>
+      <div className="flex justify-center items-center gap-8  my-6">
+            <a
+              href="mailto:your-email@example.com"
+              className="text-primary hover:text-primary-dark text-2xl"
+              aria-label="Send Email"
+            >
+              <HiOutlineMail size={50} />
+            </a>
+            <a
+              href="https://github.com/shariyerShazan"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-primary hover:text-primary-dark text-2xl"
+              aria-label="GitHub Profile"
+            >
+              <FaGithub size={50}/>
+            </a>
+            <a
+              href="https://linkedin.com/in/shariyerShazan"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-primary hover:text-primary-dark text-2xl"
+              aria-label="LinkedIn Profile"
+            >
+              <FaLinkedinIn size={50}/>
+            </a>
+            <a
+              href="https://facebook.com/darling.Shazan"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-primary hover:text-primary-dark text-2xl"
+              aria-label="Facebook Profile"
+            >
+              <FaFacebookF size={50}/>
+            </a>
+          </div>
+
+      {/* Main grid */}
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
+        
+        {/* Left grid - personal info, 2 cols inside */}
+        <div className="grid grid-cols-2 sm:grid-cols-2 gap-6">
+          {/* Name */}
+          <div>
+            <p className="font-semibold text-lg">Name:</p>
+            <p>Shariyer Shazan</p>
+          </div>
+          {/* Age */}
+          <div>
+            <p className="font-semibold text-lg">Age:</p>
+            <p>22</p>
+          </div>
+          {/* Nationality */}
+          <div>
+            <p className="font-semibold text-lg">Nationality:</p>
+            <p>Bangladeshi</p>
+          </div>
+          {/* Address */}
+          <div>
+            <p className="font-semibold text-lg">Address:</p>
+            <p>Dhaka, Bangladesh</p>
+          </div>
+          {/* Freelance */}
+          <div>
+            <p className="font-semibold text-lg">Freelance:</p>
+            <p>Available</p>
+          </div>
+          {/* Phone */}
+          <div>
+            <p className="font-semibold text-lg">Phone</p>
+            <p>+88 01724-630245</p>
+          </div>
+          
+          {/* Language */}
+          <div>
+            <p className="font-semibold text-lg">Language</p>
+            <p>Bangla, English</p>
+          </div>
+          <div>
+                  <button className="my-4  cursor-pointer group relative flex items-center  px-5 py-2 border-2 border-primary rounded-full text-primary overflow-hidden hover:text-white">
+                   <span className="ml-8"> Download CV</span>
+                    <span className=" absolute -left-[4px]  flex items-center justify-center w-11 h-full rounded-full bg-primary text-white z-10">
+                      <FaArrowRight />
+                    </span>
+                  
+                    {/* Background fill effect */}
+                    <span
+                      className="absolute top-0 left-0 w-0 h-full bg-primary transition-all duration-500 ease-in-out group-hover:w-full -z-10 rounded-full"
+                    ></span>
+                  </button>
+          </div>
+        </div>
+
+        {/* Right grid - cards */}
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-8">
+          {/* Card 1 */}
+          <div className="bg-container p-6 rounded-lg shadow-lg text-center">
+            <h3 className="text-xl font-bold mb-2">Experience</h3>
+            <p className="text-3xl font-extrabold text-primary mb-1">2+</p>
+            <p>Years Working</p>
+          </div>
+          {/* Card 2 */}
+          <div className="bg-container p-6 rounded-lg shadow-lg text-center">
+            <h3 className="text-xl font-bold mb-2">Customers</h3>
+            <p className="text-3xl font-extrabold text-primary mb-1">50+</p>
+            <p>Satisfied Clients</p>
+          </div>
+          {/* Card 3 */}
+          <div className="bg-container p-6 rounded-lg shadow-lg text-center">
+            <h3 className="text-xl font-bold mb-2">Projects </h3>
+            <p className="text-3xl font-extrabold text-primary mb-1">30+</p>
+            <p>Successful Projects</p>
+          </div>
+          {/* Card 4 */}
+          <div className="bg-container p-6 rounded-lg shadow-lg text-center">
+            <h3 className="text-xl font-bold mb-2">Awards Win</h3>
+            <p className="text-3xl font-extrabold text-primary mb-1">10+</p>
+            <p>Award Won</p>
+          </div>
+        </div>
+      </div>
+      
     </div>
   )
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { TypeAnimation } from "react-type-animation";
 import { FaArrowRight } from "react-icons/fa";
+import { Link } from "react-router";
 
 function HomePage() {
   return (
@@ -22,7 +23,7 @@ function HomePage() {
       ></div>
 
       {/* Content */}
-      <div className="relative z-10 flex flex-col md:flex-row items-center md:items-start gap-8 px-6 md:px-12 mt-[15vh] md:mt-0">
+      <div className="relative z-10 flex flex-col md:flex-row items-center  gap-8 px-6 md:px-12 mt-[15vh] md:mt-0">
         {/* Image */}
         <img
           className="w-[80%] md:w-[40%] h-[50vh] md:h-[90vh] object-cover rounded-xl shadow-lg"
@@ -31,20 +32,34 @@ function HomePage() {
         />
 
         {/* Text */}
-        <div className="text-center md:text-left">
-          <h2 className="text-3xl  md:text-5xl font-bold text-title">
-            I am <span className="text-primary">Shariyer Shazan</span>
+        <div className="text-center md:text-left flex-1 ">
+          <div className="flex justify-center items-center">
+          <div>
+          <h2 className="text-3xl  md:text-5xl font-bold text-primary ">
+            - I'm <span className="">Shariyer Shazan .</span>
           </h2>
 
           <TypeAnimation
             sequence={[
-              "Software Engineer",
+              "Software Engineer_",
               2000,
               "",
-              "MERN Stack Developer",
+              "MERN Stack Developer_",
               2000,
               "",
-              "Web Developer",
+              "Web Developer_",
+              2000,
+              "",
+              "Next.js Developer_",
+              2000,
+              "",
+              "React.js Developer_",
+              2000,
+              "",
+              "JavaScript Developer_",
+              2000,
+              "",
+              "Front-end Web Developer_",
               2000,
               "",
             ]}
@@ -54,17 +69,27 @@ function HomePage() {
             className="block text-lg md:text-xl text-primary font-semibold mt-2"
           />
 
-          <p className="mt-4 text-text text-sm md:text-base max-w-md">
-            Passionate about building modern, user-friendly, and efficient web
-            applications with clean and scalable code.
+          <p className="flex flex-col gap-4 mt-4 text-left text-text text-sm md:text-base max-w-md">
+          <span >As a passionate MERN Stack developer, I enjoy building smooth, user-friendly web experiences that solve real-world problems. With a solid grip on React.js, Next.js, Node.js, Express, and MongoDB, I specialize in creating responsive interfaces and efficient backend systems.
+          </span>
+<span className="">Alongside, I’m experienced with Tailwind CSS and GitHub, which helps me maintain clean code and collaborate effectively. I always aim to learn and grow while crafting digital products that people actually love to use.</span>
           </p>
+          </div>
+          </div>
 
-          <button className="group mt-6 flex items-center gap-3 border border-primary px-5 py-2 rounded-full transition-all duration-300 hover:bg-primary hover:text-white">
-            More About Me
-            <span className="p-2 rounded-full bg-primary text-white group-hover:bg-white group-hover:text-primary transition-all duration-300">
-              <FaArrowRight />
-            </span>
-          </button>
+          <Link to={"/about"} className="flex  justify-center items-center">
+          <button className="my-4  cursor-pointer group relative flex items-center  px-5 py-2 border-2 border-primary rounded-full text-primary overflow-hidden hover:text-white">
+ <span className="ml-8"> More About Me</span>
+  <span className=" absolute -left-[4px]  flex items-center justify-center w-11 h-full rounded-full bg-primary text-white z-10">
+    <FaArrowRight />
+  </span>
+
+  {/* Background fill effect */}
+  <span
+    className="absolute top-0 left-0 w-0 h-full bg-primary transition-all duration-500 ease-in-out group-hover:w-full -z-10 rounded-full"
+  ></span>
+</button>
+</Link>
         </div>
       </div>
     </div>
