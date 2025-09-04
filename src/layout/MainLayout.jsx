f6a3a471bb9929b5641ef1351c2dfc752cd6aa45
shariyerShazan <shariyershazan1@gmail.com>
2025-09-04 19:38:08 +0600

    updated

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,11 +1,13 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from '../componenets/Navbar'
 import { Outlet } from 'react-router'
 import ThemeSelector from '../componenets/Theme'
 import { useSelector } from 'react-redux';
+import Loader from "../componenets/Loader"
 
 function MainLayout() {
   const darkTheme = useSelector((state) => state.theme.darkTheme);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     if (darkTheme) {
@@ -14,14 +16,24 @@ function MainLayout() {
       document.documentElement.classList.remove('dark');
     }
   }, [darkTheme]);
-  
+
+  useEffect(() => {
+
+    const timer = setTimeout(() => setLoading(false), 1800);
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (loading) {
+    return <Loader />;
+  }
+
   return (
     <div className='dark:bg-gray-950 text-text'>
       <Navbar />
-       <ThemeSelector />
+      <ThemeSelector />
       <Outlet />
     </div>
   )
 }
 
-export default MainLayout
+export default MainLayout;
